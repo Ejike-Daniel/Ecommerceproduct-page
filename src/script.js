1c47b09162dc58f68b9aa7787b046b9439ab0683
Elrey009 <daniloejike@gmail.com>
2024-02-10 19:57:43 +0100

    modified script

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+// Hamburger Menu:
+
 function showSideBar() {
   document.querySelector(".hamburger").classList.toggle("hidden");
   document.querySelector(".delete").classList.toggle("hidden");
@@ -11,3 +13,30 @@ function hideSideBar() {
   document.querySelector(".navList").classList.add("hidden");
   document.querySelector(".navList").classList.remove("flex-col");
 }
+
+// Image slider:
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".slider-img");
+  slides.forEach((slide, slideIndex) => {
+    if (slideIndex === index) {
+      slide.classList.remove("hidden");
+    } else {
+      slide.classList.add("hidden");
+    }
+  });
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + 4) % 4;
+  showSlide(currentSlide);
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % 4;
+  showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
